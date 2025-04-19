0fffde58ee9c3a06f191c430adf3d7d8878655f4
chhae0222 <iiiiioio0101@naver.com>
2025-04-19 13:52:54 +0900

    코드 깔끔하게 정리

--- js/search.js
+++ js/search.js
@@ -1,12 +1,14 @@
+// 검색 버튼을 누르면 알림을 띄움 
 const search_message = () => {
     const c = '검색을 수행합니다';
     alert(c);
-    };
-    
-function googleSearch() {
-    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
-    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
-    // 새 창에서 구글 검색을 수행
-    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
+  };
+  
+  // 구글 검색
+  function googleSearch() {
+    const searchTerm = document.getElementById("search_input").value; // 검색어 가져오기
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`; // URL에 검색어 붙이기
+  
+    window.open(googleSearchUrl, "_blank"); // 새 창에서 구글 검색
     return false;
-    }
+  }
