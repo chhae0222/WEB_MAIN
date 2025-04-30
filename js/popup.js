ff317f16ccda0c6c872aacd35b3a84e9c7a56654
chhae0222 <iiiiioio0101@naver.com>
2025-04-30 13:53:17 +0900

    자바웹 9주차

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,6 @@
 // var close_time; // 시간 정보
 // var close_time2 = 10; // 10초 설정
+
 // clearTimeout(close_time); // 재호출 정지
 // close_time = setTimeout("close_window()", 10000); // 1/1000 초 지정, 바로 시작
 // show_time(); // 실시간 시간 보여주기
@@ -11,21 +12,53 @@
 //     setTimeout(show_time, 1000); //1초마다 갱신
 // }
 
-// function close_window() { // 함수 정의
-//     window.close(); // 윈도우 닫기
-// }
+function close_window() { // 함수 정의
+     window.close(); // 윈도우 닫기
+    }
 
-function pop_up() {
-    window.open(src= "../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
-}
+function setCookie(name, value, expiredays) {
+    var date = new Date();
+    date.setDate(date.getDate() + expiredays);
+    document.cookie = escape(name) + "=" + escape(value) + "; expires=" + date.toUTCString() + "; path=/" + ";SameSite=None; Secure";
+    }
 
-function over(obj) {
-    obj.src = "image/steam.svg";
-}
+    function getCookie(name) {
+        var cookie = document.cookie;
+        console.log("쿠키를 요청합니다.");
+        if (cookie != "") {
+        var cookie_array = cookie.split("; ");
+        for ( var index in cookie_array) {
+        var cookie_name = cookie_array[index].split("=");
+        if (cookie_name[0] == "popupYN") {
+        return cookie_name[1];
+        }
+        }
+        }
+        return ;
+        }    
+        
+        function closePopup() {
+            if (document.getElementById('check_popup').value) {
+            setCookie("popupYN", "N", 1);
+            console.log("쿠키를 설정합니다.");
+            self.close();
+            }
+            }            
 
-function out(obj) {
-    obj.src = "image/logo2.png";
-}
+    function pop_up() {
+       var cookieCheck = getCookie("popupYN");
+       if (cookieCheck != "N"){
+       window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+       }
+       }
+
+   function over(obj) {
+       obj.src = "image/steam.svg";
+   }
+
+   function out(obj) {
+       obj.src = "image/logo2.png";
+    }
 
 // 기억하기
 // const over = (obj) => {
